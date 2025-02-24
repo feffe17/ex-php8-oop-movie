1b188452c5c0650c625489e46636bae4c9c7ebe3
feffe1702 <fpm.asd@gmail.com>
2025-02-24 16:59:00 +0100

    add getMovieInfo method and instantiate Movie objects in index.php

--- index.php
+++ index.php
@@ -15,4 +15,17 @@ class Movie
         $this->director = $director;
         $this->genre = $genre;
     }
+
+    public function getMovieInfo()
+    {
+        return $this->title . " (" . $this->year . ") - Directed by " . $this->director . " - Genre: " . $this->genre;
+    }
 }
+
+$movie1 = new Movie("The Shawshank Redemption", 1994, array("Tim Robbins", "Morgan Freeman"), "Frank Darabont", "Drama");
+$movie2 = new Movie("The Godfather", 1972, array("Marlon Brando", "Al Pacino"), "Francis Ford Coppola", "Crime");
+
+
+echo $movie1->getMovieInfo();
+echo "<br>";
+echo $movie2->getMovieInfo();
